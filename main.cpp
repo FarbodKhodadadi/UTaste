6c0c16a1cad5639f296476111fcdc7c6e39718c1
Farbod Khodadadi <farbod.khodadadi83@gmail.com>
2024-12-21 21:33:25 +0330

    Update main and Add file's path

--- main.cpp
+++ main.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
+int main(int argc ,char* argv[]){
+
+    string restaurants_path = argv[1];
+    string districts_path = argv[2];
+
+    
+
+
+
+
+
+}
