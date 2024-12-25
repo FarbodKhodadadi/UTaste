83e3db5994e7689cebbdb1ce56e2203ff0c67107
Farbod Khodadadi <farbod.khodadadi83@gmail.com>
2024-12-25 14:46:47 +0330

    Update global.hpp

--- src/global.hpp
+++ src/global.hpp
@@ -1,1 +1,12 @@
 #include <iostream>
+#include <map>
+#include <vector>
+
+
+#include "../exeption/exception.hpp"
+#include "../exeption/badrequest_exception.hpp"
+#include "../exeption/notfound_exception.hpp"
+#include "../exeption/permission_exception.hpp"
+#include "../exeption/empty_exception.hpp"
+
+using namespace std;
