66fec83393c5385084abe27b95f0e8275b12d928
Farbod Khodadadi <farbod.khodadadi83@gmail.com>
2024-12-26 13:28:16 +0330

    Update Restaurant constructor

--- src/restaurant.cpp
+++ src/restaurant.cpp
@@ -1,7 +1,6 @@
 #include "restaurant.hpp"
 
-Restaurant::Restaurant(int num_of_tables_ ,int start_time_ ,int close_time_,
-            string district_ ,map<string,int> menu_ ,string name_):
+Restaurant::Restaurant(string name_,string district_ ,map<string,int> menu_ ,int start_time_ ,int close_time_,int num_of_tables_ ):
             num_of_tables(num_of_tables_ ),start_time(start_time_) ,close_time(close_time_),
             distirct(district_) , menu(menu_) , name(name_){
 
--- src/restaurant.hpp
+++ src/restaurant.hpp
@@ -5,8 +5,7 @@ using namespace std;
 
 class Restaurant{
 public:
-    Restaurant(int num_of_tables_ ,int start_time_ ,int close_time_,
-            string district_ ,map<string,int> menu_ ,string name_);
+    Restaurant(string name_,string district_ ,map<string,int> menu_ ,int start_time_ ,int close_time_,int num_of_tables_);
 
 
 
