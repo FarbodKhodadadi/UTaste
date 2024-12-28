40b5d96c89ad58c6d7b68965ede2c985fe510596
Farbod Khodadadi <farbod.khodadadi83@gmail.com>
2024-12-28 10:30:11 +0330

    Implement Handle command method

--- src/command.cpp
+++ src/command.cpp
@@ -1,20 +1,19 @@
 #include "command.hpp"
 
+CommandHandle::CommandHandle(vector<Restaurant*>& restaurants_,vector<District*>& districts_):
+                            restaurants(restaurants_),districts(districts_){}
 
-CommandHandle::CommandHandle(vector<Restaurant*>& restaurants_,vector<Distirict*>& distiricts_):
-                            restaurants(restaurants_),distiricts(distiricts_){}
-
-void CommandHandle::commandProsses(const string& input){
+void CommandHandle::commandProcess(const string& input){
     auto tokens = Utility::split(input,' ');
     
     string command =tokens[0];
-    if(command=GET)
+    if(command==GET)
         getCommand(tokens);
-    else if(command = PUT)
+    else if(command == PUT)
         putCommand(tokens);
-    else if(command = DELETE)
+    else if(command == DELETE)
         deleteCommand(tokens);
-    else if(command =POST)
+    else if(command ==POST)
         postCommand(tokens);
     else
         throw BadReqException(BAD_REQ);
@@ -31,11 +30,11 @@ void CommandHandle::getCommand(const vector<string> &command_line){
 void CommandHandle::postCommand(const vector<string> &command_line){
     string action=command_line[1];
 
-    if(action=SIGNUP)
+    if(action==SIGNUP)
         signup(command_line);
-    else if(action=LOGIN)
+    else if(action==LOGIN)
         login(command_line);
-    else if(action=LOGOUT)
+    else if(action==LOGOUT)
         logout(command_line);
     else
         throw NotFoundException(NOT_FOUND);
@@ -45,16 +44,13 @@ void CommandHandle::putCommand(const vector<string> &command_line){
 
 }
 void CommandHandle::signup(const vector<string>& command_line){
+
     if(command_line.size() < 7)
         throw BadReqException(BAD_REQ);
-    for(int i=0 ;i<command_line.size();i++){
-        if(command_line[i]=="username")
-            string username= command_line[i+1];
-        if(command_line[i]="password")
-            string password= command_line[i+1];
-    }
+    map<string,string> args=Utility::commandArgs(command_line);
+
     
-    
+
 }
 void CommandHandle::login(const vector<string>& command_line){}
 void CommandHandle::logout(const vector<string>& command_line){}
--- src/command.hpp
+++ src/command.hpp
@@ -1,13 +1,17 @@
 #include "global.hpp"
+#include "restaurant.hpp"
+#include "districs.hpp"
+#include "../exeption/generalException.hpp"
+#include "../general/utility.hpp"
 
 class CommandHandle{
 public:
-    CommandHandle(vector<Restaurant*>& restaurants,vector<Distirict*>& distiricts);
-    void commandProsses(const string& input);
+    CommandHandle(vector<Restaurant*>& restaurants,vector<District*>& distiricts);
+    void commandProcess(const string& input);
 
 private:
     vector<Restaurant*>& restaurants;
-    vector<Distirict*>& distiricts;
+    vector<District*>& districts;
     void deleteCommand(const vector<string>& command_line);
     void getCommand(const vector<string>& command_line);
     void postCommand(const vector<string>& command_line);
